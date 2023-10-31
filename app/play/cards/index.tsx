a6d6e79f51aceb3c8cf2e0a2e5485bb3be3d7526
r4bb1t <r4bb1t.github@gmail.com>
2023-11-01 05:06:52 +0900

    style: make cards absolute

--- app/play/cards/index.tsx
+++ app/play/cards/index.tsx
@@ -2,7 +2,7 @@ import Card from "./card";
 
 export default function Cards() {
   return (
-    <div className="w-full flex justify-center translate-y-8">
+    <div className="w-full absolute bottom-0 flex justify-center translate-y-8">
       {[...new Array(5)].map((_, i) => (
         <Card key={i} rotate={i - 2} />
       ))}
--- app/play/index.tsx
+++ app/play/index.tsx
@@ -2,8 +2,7 @@ import Cards from "./cards";
 
 export default function Play() {
   return (
-    <div className="w-full h-full bg-gray-500 flex flex-col justify-between overflow-hidden">
-      d
+    <div className="w-full h-full bg-gray-500 relative overflow-hidden">
       <Cards />
     </div>
   );
